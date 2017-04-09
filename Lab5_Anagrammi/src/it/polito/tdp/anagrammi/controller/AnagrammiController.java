f8fa8a796bce34c510cbb8f56f5d982ca6a9d5b9
Gaspare <Gaspare@192.168.1.100>
2017-04-09 12:53:07 +0200

    Versione senza controllo con Intefaccia

--- Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
@@ -1,8 +1,46 @@
+/**
+ * Sample Skeleton for 'Anagrammi.fxml' Controller Class
+ */
+
 package it.polito.tdp.anagrammi.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class AnagrammiController {
 
-	
-}
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
 
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="txtParola"
+    private TextField txtParola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="doAnagramma"
+    private Button doAnagramma; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtAnagrammiCorretti"
+    private TextArea txtAnagrammiCorretti; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtAnagrammiSbagliatii"
+    private TextArea txtAnagrammiSbagliatii; // Value injected by FXMLLoader
+
+    @FXML // fx:id="doReset"
+    private Button doReset; // Value injected by FXMLLoader
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert doAnagramma != null : "fx:id=\"doAnagramma\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert txtAnagrammiCorretti != null : "fx:id=\"txtAnagrammiCorretti\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert txtAnagrammiSbagliatii != null : "fx:id=\"txtAnagrammiSbagliatii\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert doReset != null : "fx:id=\"doReset\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+
+    }
+}
